86d981b519645180b0a89375c85133e4135a279d
Baki Emre Çetin <baki.emre.cetin@gmail.com>
2024-04-18 18:53:19 +0300

    Add project files.

---
+++ TasteFoodIt/Controllers/AdminLayoutController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+
+namespace TasteFoodIt.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        TasteContext context = new TasteContext();
+        // GET: AdminLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            ViewBag.notificaationIsReadByFalseCount = context.Notifications.Where(x => x.IsRead == false).Count();
+            var values = context.Notifications.Where(x => x.IsRead == false).ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NotificationStatusChangeToTrue(int id)
+        {
+            var values = context.Notifications.Find(id);
+            values.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("CategoryList", "Category");
+        }
+    }
+}
---
+++ TasteFoodIt/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Entities;
+using TasteFoodIt.Context;
+using System.Web.Security;
+
+namespace TasteFoodIt.Controllers
+{
+    
+    public class LoginController : Controller
+    {
+        TasteContext context = new TasteContext();
+        // GET: Login
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            var values = context.Admins.FirstOrDefault(x => x.Username == p.Username && x.Password == p.Password);
+            if (values !=null)
+            {
+                FormsAuthentication.SetAuthCookie(values.Username, true);
+                Session["a"] = values.Username;
+                return RedirectToAction("ProductList", "Product");
+            }
+            return View();
+        }
+    }
+}
